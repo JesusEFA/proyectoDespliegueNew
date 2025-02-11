b9e0200597667da61926910959e27cd8b3aed0a4
JesusEFA <94001781+JesusEFA@users.noreply.github.com>
2025-02-11 14:09:52 +0100

    Add files via upload

---
+++ src/test/java/com/desarrolloservidor/pruebaTransversal/PruebaDespliegueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrolloservidor.pruebaTransversal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaDespliegueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
